430ddef99c7b2b33beeb80cfb1d96c97cb4459d6
KhuloodGhazi <khulood.gmo@gmail.com>
2025-05-07 14:59:20 +0000

    fix

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import sys
-str = sys.stdin.read()
-print(str[39:66] + str[106:112] + str[0:6])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[0:6]
+print(str)
 
