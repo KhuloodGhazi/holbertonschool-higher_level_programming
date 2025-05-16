4b9506745177233df58c308099b243c0638f418c
KhuloodGhazi <khulood.gmo@gmail.com>
2025-05-16 11:50:21 +0000

    fix 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python3
 def safe_print_division(a, b):
+    result = None
     try:
         result = a / b
-        print("Inside result: {:.1f}".format(result))
-    except:
-        result = None
-        print("Inside result: {}".format(result))
+    except Exception:
+        pass
     finally:
-        return result
+        print("Inside result: {}".format(result))
+    return result
