489fa81b44a261eeeef434a864e89485785bcf6c
KhuloodGhazi <khulood.gmo@gmail.com>
2025-05-27 14:53:38 +0000

    fix task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-"""
-===========================
-Module with class MyList
-===========================
-"""
+"""defines class MyList"""
 
 
 class MyList(list):
-    """Class with method print_sorted"""
-    pass
+    """class that inherits from list
+    with public instance method to print sorted list"""
 
     def print_sorted(self):
-        """Methot that sorted a list"""
-
-        print(sorted(list(self)))
+        print(sorted(self))
